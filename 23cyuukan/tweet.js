90b7f5714e5a7f3099c2e2376c5ee46adee13b0a
test <m0a22236c6@edu.teu.ac.jp>
2023-11-28 18:00:09 +0900

    Commit Message

--- 23cyuukan/tweet.js
+++ 23cyuukan/tweet.js
@@ -1,10 +1,57 @@
 let answer= document.querySelector('#answer');
-let outputHtml ='<ul>';
 
-let i = 0;
-while (i<tweets.length) {
-  outputHtml += '<li><img src="${tweets[i].avatar}"> ${tweets[i].name} ${tweets[i].message}</li>';
-  i++;
+changeAll();
+
+//ボタン全表示
+function changeAll() {
+  let outputHtml = '<ul>';
+  let i = 0;
+  while (i < tweets.length) {
+    outputHtml += '<li style="list-style:none"> <img src="${tweets[i].avatar}" alt=""> <b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i> </li>';
+    i++;
+  }
+  outputHtml += '</ul>';
+  answer.innerHTML = outputHtml;
 }
-outputHtml+='</ul>';
-answer.innerHTML =outputHtml;
+
+//三郎BOTのみ表示
+function changeBot(){
+  let outputHtml = '<ul>';
+  let i = 0;
+  while(i<tweets.length){
+    if (tweets[i].name === "三郎BOT"){
+      outputHtml += '<li style="list-style:none"><img src="${tweets[i].avatar}" alt=""> <b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i></li>';
+    }
+    i++;
+  }
+  outputHtml+='</ul>';
+  answer.innerHTML = outputHtml;
+}
+
+//次郎のみ表示
+function changeJiro(){
+  let outputHtml='<ul>';
+  let i = 0;
+  while(i<tweets.length){
+    if (tweets[i].name==="次郎"){
+      outputHtml += '<li style="list-style:none"><img src="${tweets[i].avatar}" alt=""><b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i></li>';
+    }
+    i++;
+  }
+  outputHtml+='</ul>';
+  answer.innerHTML = outputHtml;
+}
+
+//太郎のみ表示
+function changeTaro(){
+  let outputHtml='<ul>';
+  let i = 0;
+  while(i<tweets.length){
+    if (tweets[i].name==="太郎"){
+      outputHtml += '<li style="list-style:none"><img src="${tweets[i].avatar}" alt=""> <b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i></li>';
+    }
+    i++;
+  }
+  outputHtml+='</ul>';
+  answer.innerHTML = outputHtml;
+}
